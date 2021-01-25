89a2b3c77246cbe5b20a8fe3bcf7bf0b2c2e412f
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-01-25 16:30:44 +0530

    local settings import

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -141,3 +141,16 @@ from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger'
 }
+
+#Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'example@gmail.com'
+EMAIL_HOST_PASSWORD = 'ownoofoifgoeokekeeoengongo'
+EMAIL_USE_TLS = True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
+
