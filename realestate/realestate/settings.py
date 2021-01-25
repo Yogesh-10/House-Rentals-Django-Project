b23dce2a96a5dc741605bb0bed39259f903eb86e
Yogesh-10 <yogiyogesh100800@gmail.com>
2021-01-26 02:19:18 +0530

    Heroku setup

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path,os
+import django_heroku
 import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -141,6 +142,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'realestate/static')
 ]
+django_heroku.settings(locals())
 
 #Media Folders
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
