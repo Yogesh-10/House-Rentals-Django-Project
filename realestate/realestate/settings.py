e4203a17b1050ab4d0b62dc288ac69d86b8c6286
Yogesh <you@example.com>
2021-01-26 07:05:09 +0530

    ready for deploy

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path,os
-import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-env = environ.Env()
 # reading .env file
-environ.Env.read_env()
 
 SECRET_KEY='hvtyct5r5r#@#$#$&vtycty65fd56d5665#^&xy'
 
