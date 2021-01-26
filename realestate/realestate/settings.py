71c1e7838014531297cb08d9e82814cbd295b997
Yogesh <you@example.com>
2021-01-26 13:37:38 +0530

    Serve static files

--- realestate/realestate/settings.py
+++ realestate/realestate/settings.py
@@ -141,7 +141,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'realestate/static')
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 #Media Folders
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
